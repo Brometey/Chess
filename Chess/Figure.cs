4dba29c919fe918d9a68ea558a7df6b6b13fce89
Brometey <barstar35@gmail.com>
2022-06-06 13:05:43 +0300

    Добавьте файлы проекта.

---
+++ Chess/Figure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chess
+{
+    enum Figure
+    {
+        none, 
+
+        whiteKing = 'K',
+        whiteQueen = 'Q',
+        whiteRock = 'R',
+        whiteBishop = 'B',
+        whiteKnight = 'N',
+        whitePawn = 'P',
+
+        blackKing = 'k',
+        blackQueen = 'q',
+        blackRock = 'r',
+        blackBishop = 'b',
+        blackKnight = 'n',
+        blackPawn = 'p',
+
+    }
+}
---
+++ ChessDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ChessLibrary;
+
+namespace ChessDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessLibrary.Chess chess = new ChessLibrary.Chess();
+            while (true)
+            {
+                Console.WriteLine(chess.Fen);
+                string move = Console.ReadLine();
+                if (move == "") break;
+                chess = chess.Move(move);
+            }
+
+        }
+    }
+}
