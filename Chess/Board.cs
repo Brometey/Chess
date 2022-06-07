5da289756da979b3158b70313d26471b59742041
Brometey <barstar35@gmail.com>
2022-06-07 11:36:11 +0300

    Расставил фигуры по доске методом InitFigures().

--- Chess/Board.cs
+++ Chess/Board.cs
@@ -20,8 +20,29 @@ namespace ChessLibrary
 
         void Init()
         {
-            SetFigureAt(new Square("a1"), Figure.whiteKing);
-            SetFigureAt(new Square("h8"), Figure.blackKing);
+            // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+            // 0                                           1 2    3 4 5
+            string[] parts = Fen.Split();
+            if (parts.Length != 6) return;
+            InitFigures(parts[0]);
+            MoveColor = (parts[1] == "b") ? Color.black : Color.white; 
+            MoveNumber = int.Parse(parts[5]);
+ 
+        }
+
+        private void InitFigures(string data)
+        {
+            for (int j = 8; j >= 2; j--)
+            {
+                data = data.Replace(j.ToString(),(j-1).ToString() + "1");
+            }
+            data = data.Replace("1", ".");
+            string[] lines = data.Split('/');
+            for (int y = 7; y >= 0; y--)
+                for (int x = 0; x < 8; x++)
+                    figures[x, y] = (Figure)lines[7 - y][x];
+            
+            
         }
 
         public Figure GetFigureAt(Square square)
